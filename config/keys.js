43408d239cbba5813796c9bc36e5680fad7d7833
minicola23 <minicola23@gmail.com>
2018-06-28 17:32:41 +0700

    add key.js

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// https://console.developers.google.com/
+module.exports = {
+    googleClientID: '1074838298695-ogmos6gd3pl2ebqlrv6s4u0nmjbl6ht5.apps.googleusercontent.com',
+    googleClientSecret: 'LbIICgVAwQvCKq3i8QAtPOAa'
+};
